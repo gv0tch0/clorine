67e3997d6c149b1197e085f7d0b6e9c6e17989c7
Relay Network <kburton@relaynetwork.com>
2011-01-18 13:18:40 -0500

    bump version to 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.relaynetwork/clorine "1.0.0-SNAPSHOT"
+(defproject com.relaynetwork/clorine "1.0.0"
   :description "Clorine"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
